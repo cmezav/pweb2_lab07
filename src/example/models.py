e9d6e0c025618bef8dd8537e5d719c2a78933519
CieloMV <cmezav@unsa.edu.pe>
2023-07-11 17:27:06 -0500

    Creando modelo 'Simple' y registrandolo en admin

--- src/example/models.py
+++ src/example/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Simple(models.Model):
+    text = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
+    url = models.URLField(default='www.example.com')
+
+    def __str__(self):
+        return self.url
