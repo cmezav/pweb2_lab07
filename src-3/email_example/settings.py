85a3f1e557d5ff8558b60f18bdd21da56c37732b
CieloMV <cmezav@unsa.edu.pe>
2023-07-12 02:33:42 -0500

    Envío de email

--- src-3/email_example/settings.py
+++ src-3/email_example/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'cmv151219@gmail.com'
+EMAIL_HOST_PASSWORD = 'djangopassword7'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- src-3/send/views.py
+++ src-3/send/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 def index(request):
+
+    send_mail('Hello from Django',
+    'Hello there. This is an automated message.',
+    'cmv151219@gmail.com',
+    ['cmezav@unsa.edu.pe'],
+    fail_silently=False)
+    
     return render(request, 'send/index.html')
+
